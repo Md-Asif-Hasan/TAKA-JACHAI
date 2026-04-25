60367ffc5924a6af51455f7ca8344d5e7a3f6f0e
Md Asif Hasan <127124566+Md-Asif-Hasan@users.noreply.github.com>
2026-04-25 12:29:12 +0600

    Delete TAKA JACHAI website directory

--- TAKA JACHAI website/js/privacy.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Privacy policy page loaded');
-    
-    const externalLinks = document.querySelectorAll('a[href^="http"]');
-    externalLinks.forEach(link => {
-        link.setAttribute('target', '_blank');
-        link.setAttribute('rel', 'noopener noreferrer');
-    });
-});
